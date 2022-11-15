d70e1a48ba406fab49bf798f7d55d84e0f128fee
Radoslaw <zerek.r93@gmail.com>
2022-11-15 15:20:52 +0100

    Initial commit

---
+++ src/main/java/org/radoslawzerek/cafemanagementsystem/CafeManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package org.radoslawzerek.cafemanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeManagementSystemApplication.class, args);
+	}
+
+}
